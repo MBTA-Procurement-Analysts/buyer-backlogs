53e94009b6c4251d4838fc91c1d4419692d9888c
Nianmin Guo <nguo@MBTA.com>
2018-10-19 10:44:50 -0400

    Timemachine trial

--- time_machine-graphs.r
+++ time_machine-graphs.r
@@ -18,7 +18,7 @@ db_url <- if (Sys.info()[[1]] == "Linux") {"mongodb://127.0.0.1:27017"} else {"m
 
 mongo_approval_worklist <- mongo(collection = "timemachine_pp_worklist", db = "test", url = db_url)
 
-# Export to temp file 
+# Export to temp file
 mongo_approval_worklist$export(file("approval_worklist_timemachine.json"))
 
 # Read temp file as Dataframe
@@ -28,8 +28,21 @@ approval_worklist_timemachine_json <- stream_in(file("approval_worklist_timemach
 approval_worklist_timemachine <- as_tibble(approval_worklist_timemachine_json[2:11])
 
 # (WIP)
-approval_worklist_timemachine <- approval_worklist_timemachine %>% mutate("50K+" = Sum_of_PO_Amt > 50000) %>% group_by(`50K+`, Archive_Time) %>% summarize(Cnt = n())
+approval_worklist_timemachine <- approval_worklist_timemachine %>% 
+  mutate("50K+" = if_else(Sum_of_PO_Amt > 50000, "PP", "Total")) %>% 
+  group_by(`50K+`, Archive_Time) %>% 
+  summarize(Cnt = n()) %>% 
+ # mutate_at("Archive_Time", ymd) %>% 
+  group_by(Archive_Time) %>%
+  mutate(Cnt = if_else(`50K+` == "Total", sum(Cnt), Cnt)) %>% 
+  spread(., "50K+", Cnt) %>% 
+  ungroup(Archive_Time)
+  
 
+plot_ly(data = approval_worklist_timemachine) %>% 
+  add_lines(x = ~Archive_Time, y = ~PP, name = "PP's List (> $50K)") %>% 
+  add_lines(x = ~Archive_Time, y = ~Total, name = "Total") %>% 
+  layout(xaxis = list(title = "Date"), yaxis = list(title = "Count")) 
 # A tibble: 2 x 3
 # Groups:   50K+ [?]
 #`50K+` Archive_Time   Cnt
