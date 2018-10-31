fd0a14c63880fe4a3e6b2f98f759e1ff981a4418
Nianmin Guo <nguo@MBTA.com>
2018-10-31 15:59:51 -0400

    Settled Req JSON order issue for now

--- rubix_getter.r
+++ rubix_getter.r
@@ -28,7 +28,14 @@ get.req.tibble <- function (poNum) {
     {
       po_request <- curl_fetch_memory(str_glue("{api_base_url}po/{poNum}"))
       po_json <- fromJSON(rawToChar(po_request$content))
-      req_num <- trimws(po_json$lines[[1]][1,]$Requisition[[2]])
+      # It has been observed that the Order of variables in the JSON response
+      #   is not stable (i.e., Req ID will be in [[1]] or [[2]], randomly).
+      # The other field in the collection is the Req Line #.
+      # Thus, with some rage-filled debug session, the call to get Req ID is 
+      #   shaped below.
+      req_num <- trimws(if_else(is.character(po_json$lines[[1]][1,]$Requisition[[1]]),
+                                po_json$lines[[1]][1,]$Requisition[[1]],
+                                as.character(po_json$lines[[1]][1,]$Requisition[[2]])))
       req_request <- curl_fetch_memory(str_glue("{api_base_url}req/{req_num}"))
       req_json <- fromJSON(rawToChar(req_request$content))
       req_approval_date <- date(parse_date_time(substr(req_json$Approved_On, 1, 10), "%Y-%m-%d"))
@@ -39,10 +46,11 @@ get.req.tibble <- function (poNum) {
     error = function(cond) {
       print(cond)
       print(str_glue("Problem Getting PO info of {poNum}, it might not exist in Rubix DB."))
-      # req_tibble <- tibble(`req_description` = "", `req_approval_date` = NA, `req_requester` = "") 
+      #req_tibble <- tibble(`req_description` = "", `req_approval_date` = NA, `req_requester` = "") 
     }
   )
   # return
+  print(req_tibble)
   req_tibble
 }
 
@@ -50,6 +58,7 @@ get.req.tibble <- function (poNum) {
 # Wrapper for batch running get.req.table 
 get.reqs.tibble <- function (poNums) {
   print(poNums)
-  print(map(poNums, get.req.tibble))
+  #print(map(poNums, get.req.tibble))
   bind_rows(map(poNums, get.req.tibble))
 }
+ 
