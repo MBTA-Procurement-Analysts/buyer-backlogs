f6041a6e215d295931156b77014c5e6c359d4779
Nianmin Guo <nguo@MBTA.com>
2019-11-12 10:41:32 -0500

    New B-Logic for Approver Throughput

--- approver-throughput.r
+++ approver-throughput.r
@@ -13,16 +13,59 @@ approver_historical_data_raw <- read_excel(approver_historical_data_path, skip =
 
 # Data Wrangling ----------------------------------------------------------
 
-approver_historical_data <- approver_historical_data_raw %>% 
+# Tidy Procedures:
+# - Drop unnecessary columns (User, Available_DTTM, Unit, Origin, Instance,
+#     Selected_DTTM)
+# - Filter by last 3 months by Worked_DTTM, and Status of 2
+# - Dealing with PO Line with multiple observations of different amounts:
+#   - Group by all but amount, created new column of max amount per PO Line
+#   - Drop Amount column, this will make duplicate PO Lines have the same
+#       (max) amount
+#   - distinct() on all 
+# - Dealing with multiple buyers in the same PO:
+#   - Sort by PO# and Descending Worked_DTTM
+#   - distinct() by OperID (BuyerID)
+#   * This workes since distinct() keeps only first found instance
+
+approver_historical_data_all <- approver_historical_data_raw %>% 
+  select(-User, -Available_DTTM, -Unit, -Origin, -Instance, -Selected_DTTM) %>% 
   filter(`Worked_DTTM` >= (data_date - months(3)), Status == 2) %>% 
+  group_by_at(vars(everything(), -Amount)) %>% 
+  summarise(Max_Amount = max(Amount)) %>% 
+  ungroup() %>% 
+  arrange(`PO No.`, desc(Worked_DTTM)) %>% 
+  distinct(Status, `PO No.`, `Line`, `More Info`, `Max_Amount`, .keep_all=TRUE)
+  
+# Mutate Procedures:
+# - Extract Date from Worked_DTTM (downstream function can't take timestamps)
+# - Calculate the Sunday of the week, formats it as string
+# - Unselect Line Specific information, calculate sum amount for PO
+approver_historical_data_all <- approver_historical_data_all %>% 
   mutate(Date = date(`Worked_DTTM`)) %>% 
   mutate(`WeekNo` = date.of.week(`Date`)) %>% 
   mutate_at("WeekNo", ~format(., "%m/%d/%y")) %>% 
-  select(-Line, -`More Info`, -Amount, -`Worked_DTTM`, -Instance, -Selected_DTTM) %>% 
-  distinct()
+  select(-Line, -`More Info`, -`Worked_DTTM`) %>% 
+  group_by_at(vars(everything(), -Max_Amount)) %>% 
+  summarise(Total_Amount = sum(Max_Amount)) %>% 
+  ungroup()
 
-approver_historical_tibble <- approver_historical_data %>% 
+# Split All from Over 50k 
+approver_historical_data_over50k <- approver_historical_data_all %>% 
+  filter(Total_Amount > 500000)
+
+
+approver_historical_tibble_all <- approver_historical_data_all %>%
   group_by(WeekNo) %>%
   summarize(Cnt = n()) %>% 
   spread(`WeekNo`, `Cnt`) %>% 
   replace(is.na(.), 0)
+
+approver_historical_tibble_over50k <- approver_historical_data_over50k %>% 
+  group_by(WeekNo) %>%
+  summarize(Cnt = n()) %>% 
+  spread(`WeekNo`, `Cnt`) %>% 
+  replace(is.na(.), 0)
+
+approver_historical_tibble <- 
+  bind_rows(approver_historical_tibble_all, approver_historical_tibble_over50k) %>% 
+  bind_cols(tibble(" " = c("All", "50k+")), .)
